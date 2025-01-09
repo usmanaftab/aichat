1318394228e5fba3d1c475eabc2d5f54c516cc02
Usman Aftab <usman.aftab@gmail.com>
2025-01-09 00:18:54 +0000

    breadcrumbs

--- src/pages/dashboard/components/NavbarBreadcrumbs.tsx
+++ src/pages/dashboard/components/NavbarBreadcrumbs.tsx
@@ -3,6 +3,9 @@ import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import Breadcrumbs, { breadcrumbsClasses } from '@mui/material/Breadcrumbs';
 import NavigateNextRoundedIcon from '@mui/icons-material/NavigateNextRounded';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link } from '@mui/material';
 
 const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
   margin: theme.spacing(1, 0),
@@ -15,14 +18,41 @@ const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
   },
 }));
 
+interface BreadcrumbItem {
+  label: string;
+  href: string;
+}
+
 export default function NavbarBreadcrumbs() {
+  const [breadcrumbs, setBreadcrumbs] = React.useState<BreadcrumbItem[]>([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    const pathSegments = location.pathname.split('/').filter(Boolean); 
+    const createBreadcrumb = (path: string, label: string): BreadcrumbItem => ({
+      href: `/${path}`, 
+      label,
+    });
+
+    const newBreadcrumbs: BreadcrumbItem[] = [];
+    newBreadcrumbs.push(createBreadcrumb('/', 'Dashboard'));
+    pathSegments.forEach((segment, index) => {
+      const path = pathSegments.slice(0, index + 1).join('/');
+      const label = segment.charAt(0).toUpperCase() + segment.slice(1); // Capitalize first letter
+      newBreadcrumbs.push(createBreadcrumb(path, label));
+    });
+
+    setBreadcrumbs(newBreadcrumbs);
+  }, [location.pathname]);
   return (
+
     <StyledBreadcrumbs
       aria-label="breadcrumb"
       separator={<NavigateNextRoundedIcon fontSize="small" />}
     >
-      <Typography variant="body1">Dashboard</Typography>
-      <Typography variant="body1" sx={{ color: 'text.primary', fontWeight: 600 }}>AI Chat</Typography>
+      {breadcrumbs.map((item, index) => (
+        index === breadcrumbs.length - 1 ? <Typography variant="body2" sx={{ color: 'text.primary', fontWeight: '500' }}> {item.label} </Typography> : <Typography variant="body2" > {item.label} </Typography>
+      ))}
     </StyledBreadcrumbs>
   );
 }
